a2bea5143f2a4c5b822adf702408b5e1df699039
lu <lugege9266@gmail.com>
2019-06-19 15:07:03 +0800

    fix bug

--- app.js
+++ app.js
@@ -4,48 +4,11 @@ var mysql = require('mysql');
 var connection = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '1',
+    password: 'A1140910124',
     database: 'DATALIST'
 });
 
 connection.connect();
-
-const scheduleCronstyle = () => {
-    //每分钟的第30秒定时执行一次:
-    schedule.scheduleJob('30 * * * * *', () => {
-        UpdateDataBase();
-    });
-}
-
-scheduleCronstyle();
-
-function UpdateDataBase() {
-    var sql = `DELETE FROM dataList`;
-    connection.query(sql, (error, results, fields) => {
-        if (error)
-            return console.error(error.message);
-
-        console.log('clear data');
-
-        var config = {
-            page: 1,
-            skip: 0,
-            include: ['title', 'imgUrl', 'desc', 'downloadLink', 'descPageLink']
-        };
-
-        setTimeout(function() {
-            dyttReptitle(config).then(result => {
-                var addSql = 'INSERT INTO dataList(title,imgUrl,`desc`,downloadLink,descPageLink) VALUES ?';
-                var values = dealData(result);
-                connection.query(addSql, [values], function(err, result) {
-                    if (err) throw err;
-                    console.log('reset data');
-                });
-            });
-        }, 2000);
-
-    });
-}
 
 function dealData(data) {
     var result = [];
@@ -54,3 +17,36 @@ function dealData(data) {
     });
     return result;
 }
+
+const scheduleCronstyle = () => {
+    //每分钟的第30秒定时执行一次:
+    schedule.scheduleJob('30 * * * * *', () => {
+        var sql = `DELETE FROM dataList`;
+        connection.query(sql, (error, results, fields) => {
+            if (error)
+                return console.error(error.message);
+
+            console.log('clear data');
+
+            var config = {
+                page: 1,
+                skip: 0,
+                include: ['title', 'imgUrl', 'desc', 'downloadLink', 'descPageLink']
+            };
+
+            setTimeout(function() {
+                dyttReptitle(config).then(result => {
+                    var addSql = 'INSERT INTO dataList(title,imgUrl,`desc`,downloadLink,descPageLink) VALUES ?';
+                    var values = dealData(result);
+                    connection.query(addSql, [values], function(err, result) {
+                        if (err) throw err;
+                        console.log('reset data');
+                    });
+                });
+            }, 2000);
+
+        });
+    });
+}
+
+scheduleCronstyle();
